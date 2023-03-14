a04f42edae65c4255420cd2c728c8b315711e990
anthow <anthonyenglebert@gmail.com>
2023-03-14 14:48:51 +0100

    /

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,11 +4,11 @@
 		<h2 class="text-2xl text-center font-black mb-5">Revenus individuels</h2>
 		<ul class="text-lg">
 			<li>Laura Laruelle:<br /> 0 €</li>
-			<li>Julie Vanderbyse:<br /> 228,67 €</li>
+			<li>Julie Vanderbyse:<br /> 0 €</li>
 			<li>Anthony Englebert:<br /> 0 €</li>
 			<li>David Hainaut:<br /> 500 €</li>
-			<li>Jeremy Scala: <br /> 375,23 €</li>
-			<li>Thomas Dussart: <br /> 228,67 €</li>
+			<li>Jeremy Scala: <br /> 0 €</li>
+			<li>Thomas Dussart: <br /> 0 €</li>
 			<li>Christophe Renard: <br /> 876,37 €</li>
 		</ul>
 	</section>
@@ -17,20 +17,18 @@
 		<h2 class="text-2xl text-center font-black mb-5">Revenus en attente</h2>
 		<ul class="text-lg">
 			<li>Laura Laruelle:<br /> 0 €</li>
-			<li>Julie Vanderbyse:<br /> 883,57 €</li>
-			<li>Anthony Englebert:<br /> 3805,25</li>
+			<li>Julie Vanderbyse:<br /> 0 €</li>
+			<li>Anthony Englebert:<br /> 0 €</li>
 			<li>David Hainaut:<br /> 1071,03 €</li>
-			<li>Jeremy Scala: <br /> 883,57</li>
-			<li>Thomas Dussart: <br /> 883,57</li>
+			<li>Jeremy Scala: <br /> 0 €</li>
+			<li>Thomas Dussart: <br /> 0 €</li>
 			<li>Christophe Renard: <br /> 1539,63 €</li>
 		</ul>
 	</section>
 
 	<section class="bg-third text-white h-min p-4 rounded">
 		<h2 class="text-2xl text-center font-black mb-5">Etat du compte Smart</h2>
-		<h3 class="text-2xl text-center font-black mb-10">2864,23  €</h3>
-		<h2 class="text-2xl text-center font-black mb-5">Budget Coop</h2>
-		<h3 class="text-2xl text-center font-black mb-10">241,68 €</h3>
+		<h3 class="text-2xl text-center font-black mb-10">1904,11  €</h3>
 	</section>
 
 	<section class="bg-third h-min text-white p-4 rounded">
